ed5fa31b006eda1cde44734f5d5b099572438819
aziz kacem <155546839+azizkcm09@users.noreply.github.com>
2025-10-17 00:22:48 +0100

    render projects

--- src/Project.jsx
+++ src/Project.jsx
@@ -2,8 +2,38 @@ import React from "react";
 import { useFetchProjects } from "./fetchProjects";
 const Project = () => {
   const { loading, projects } = useFetchProjects();
-  console.log(loading, projects);
-  return <h2>Project</h2>;
+  if (loading) {
+    return (
+      <section className="projects">
+        <h2>Loading...</h2>
+      </section>
+    );
+  }
+  return (
+    <section className="projects">
+      <div className="title">
+        <h2>projects</h2>
+        <div className="title-underline"></div>
+      </div>
+      <div className="projects-center">
+        {projects.map((project) => {
+          const { title, url, id, img } = project;
+          return (
+            <a
+              href={url}
+              key={id}
+              className="project"
+              target="_blank"
+              rel="noreferrer"
+            >
+              <img src={img} alt={title} className="img" />
+              <h5>{title}</h5>
+            </a>
+          );
+        })}
+      </div>
+    </section>
+  );
 };
 
 export default Project;
